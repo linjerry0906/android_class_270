e2ba17e6126ee349003e647ec0dbba2cb481cb81
linjerry0906 <linjerry0906@yahoo.com.tw>
2016-07-13 10:44:07 +0800

    add listview and spinner

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     EditText editText;
     RadioGroup radioGroup;
+    ListView listView;
+    Spinner spinner;
 
     String selectedTea = "black tea";
 
@@ -27,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView)findViewById(R.id.textView);
         editText = (EditText)findViewById(R.id.editText);
         radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        listView = (ListView)findViewById(R.id.listView);
+        spinner = (Spinner)findViewById(R.id.spinner);
 
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
@@ -42,16 +49,26 @@ public class MainActivity extends AppCompatActivity {
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId)
-            {
-               RadioButton radioButton = (RadioButton)group.findViewById(checkedId);
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
                 selectedTea = radioButton.getText().toString();
             }
         });
-
+    setupListView();
+    setupSpinner();
     }
-
-
+    public void setupListView()
+    {
+        String[] data = new String[]{"black tea", "green tea", "1", "2", "3", "4", "5"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        listView.setAdapter(adapter);
+    }
+    public void setupSpinner()
+    {
+        String[] data = getResources().getStringArray(R.array.storeInfos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, data);
+        spinner.setAdapter(adapter);
+    }
     public void submit(View view)
     {
         String text = editText.getText().toString();
