672fe325fe5c7c45138988588c3b13832c9f01fa
linjerry0906 <linjerry0906@yahoo.com.tw>
2016-07-14 11:00:44 +0800

    Add Drink Item View

--- SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
@@ -1,25 +1,55 @@
 package com.example.user.simpleui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DrinkMenuActivity extends AppCompatActivity {
 
     TextView totalTextView;
     ListView drinkMenuListView;
 
+    String[] names = {"冬瓜紅茶", "玫瑰鹽奶蓋紅茶", "珍珠紅茶拿鐵", "紅茶拿鐵"};
+    int[] mPrices = {25, 35, 45, 35};
+    int[] lPrices = {35, 45, 55, 45};
+    int[] imageId = {R.drawable.blackTea, R.drawable.greenTea, R.drawable.milkTea, R.drawable.coffee};
+
+    List<Drink> drinks = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_menu);
         Log.d("Debug", "DrinkMenuActivity OnCreate");
+        setData();
 
         totalTextView = (TextView)findViewById(R.id.totalTextView);
         drinkMenuListView = (ListView)findViewById(R.id.drinkMenuListView);
+    }
+    public void setData()
+    {
+        for(int i = 0; i < names.length; i++)
+        {
+            Drink drink = new Drink();
+            drink.name = names[i];
+            drink.mPrice = mPrices[i];
+            drink.lPrice = lPrices[i];
+            drink.imageId = imageId[i];
+            drinks.add(drink);
+        }
+    }
+
+    public void cancel()
+    {
+        Intent intent = new Intent();
+        intent.setClass(this, MainActivity.class);
+
     }
     @Override
     protected void onStart() {
