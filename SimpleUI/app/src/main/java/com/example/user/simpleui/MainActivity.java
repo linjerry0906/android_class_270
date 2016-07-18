0d3591671649364e2834fa5262e2abea5a20bfe5
linjerry0906 <linjerry0906@yahoo.com.tw>
2016-07-18 11:35:45 +0800

    change order drinkName to menuResults and add sharePreference to store editText

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.simpleui;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -32,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
     String selectedTea = "black tea";
 
+    String menuResults = "";
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
     List<Order> orders = new ArrayList<>();
 
     @Override
@@ -46,9 +52,16 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView)findViewById(R.id.listView);
         spinner = (Spinner)findViewById(R.id.spinner);
 
+        sharedPreferences = getSharedPreferences("setting", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        
+        editText.setText(sharedPreferences.getString("editText", ""));
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
+                String text = editText.getText().toString();
+                editor.putString("editText", text);
+                editor.commit();
                 if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
                     submit(v);
                     return true;
@@ -88,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
         Order order = new Order();
         order.note = text;
-        order.drinkName = selectedTea;
+        order.menuResults = menuResults;
         order.storeInfo = (String)spinner.getSelectedItem();
 
         orders.add(order);
@@ -96,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
         setupListView();
 
         editText.setText("");
+        menuResults = "";
     }
 
     public void goToMenu(View view)
@@ -115,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
             {
                 Toast.makeText(this, "完成菜單", Toast.LENGTH_SHORT).show();
 
-                textView.setText(data.getStringExtra("results"));
+                menuResults = (data.getStringExtra("results"));
             }
             else if(resultCode == RESULT_CANCELED)
             {
